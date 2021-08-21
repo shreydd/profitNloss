1829f716dec118627426914b1a2c363379e879d9
shreydd <47713047+shreydd@users.noreply.github.com>
2021-08-21 08:12:42 +0000

    Updated Css

--- script.js
+++ script.js
@@ -3,22 +3,26 @@ var stocksQuantity = document.querySelector('#stocks-quantity');
 var currentPrice = document.querySelector('#current-price');
 var submitBtn = document.querySelector('#submit-btn');
 var outputBox = document.querySelector('#output-box');
+var profitImage = document.querySelector('.profitImage');
 
 function calculateProfitAndLoss(initial, quantity, current) {
   if(initial > current){
     // loss logic here
     var loss = (initial - current) * quantity;
     var lossPercentage = (loss/initial) * 100;
+    profitImage.style.visibility = 'hidden';
     showOutput(`You have incured a loss of ${loss} with ${lossPercentage}% loss percentage`)
   }
   else if (current > initial) {
     //profit logic here
     var profit = (current - initial) * quantity;
     var profitPercentage = (profit / initial) * 100;
+    profitImage.style.visibility = "visible";
     showOutput(`You made a profit of ${profit} with ${profitPercentage}% returns`);
   }else {
     // No loss no gain
-    showOutput(`Hodl!!!!!!! or sell!!!!!`);
+    profitImage.style.visibility = 'hidden';
+    showOutput(`No pain no gain`);
   }
 }
 
